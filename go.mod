5cc20e9bd0dd6362cd6ce17014299814749fe430
ICHINOSE Shogo <shogo82148@gmail.com>
2024-04-15 23:12:44 +0900

    add level and version option

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/shogo82148/qrcode-generator
 
-go 1.21.4
+go 1.22.2
 
 require github.com/shogo82148/qrcode v0.1.1
 
--- main.go
+++ main.go
@@ -4,28 +4,91 @@ import (
 	"bytes"
 	"image/png"
 	"net/http"
+	"strconv"
 
 	"github.com/shogo82148/qrcode"
 	"github.com/shogo82148/ridgenative"
 )
 
 func main() {
-	http.HandleFunc("/qr", func(w http.ResponseWriter, r *http.Request) {
-		data := []byte(r.URL.Query().Get("data"))
-		img, err := qrcode.Encode(data, qrcode.WithLevel(qrcode.LevelL))
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		var buf bytes.Buffer
-		err = png.Encode(&buf, img)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
+	g := NewGenerator()
+	ridgenative.ListenAndServe(":8080", g)
+}
 
-		w.Header().Set("Content-Type", "image/png")
-		w.Write(buf.Bytes())
-	})
-	ridgenative.ListenAndServe(":8080", nil)
+type Generator struct {
+	mux *http.ServeMux
+}
+
+func NewGenerator() *Generator {
+	mux := http.NewServeMux()
+	g := &Generator{
+		mux: mux,
+	}
+	mux.HandleFunc("GET /qr", g.getQR)
+	return g
+}
+
+func (g *Generator) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	g.mux.ServeHTTP(w, r)
+}
+
+func (g *Generator) getQR(w http.ResponseWriter, r *http.Request) {
+	q := r.URL.Query()
+	data := q.Get("data")
+	opts := make([]qrcode.EncodeOptions, 0, 4)
+
+	if q.Has("level") {
+		level := q.Get("level")
+		switch level {
+		case "L", "l":
+			opts = append(opts, qrcode.WithLevel(qrcode.LevelL))
+		case "M", "m":
+			opts = append(opts, qrcode.WithLevel(qrcode.LevelM))
+		case "Q", "q":
+			opts = append(opts, qrcode.WithLevel(qrcode.LevelQ))
+		case "H", "h":
+			opts = append(opts, qrcode.WithLevel(qrcode.LevelH))
+		default:
+			http.Error(w, "invalid level", http.StatusBadRequest)
+			return
+		}
+	}
+
+	version := qrcode.Version(0)
+	if q.Has("version") {
+		v, err := strconv.Atoi(q.Get("version"))
+		if err != nil {
+			http.Error(w, "invalid version", http.StatusBadRequest)
+			return
+		}
+		if v < 1 || v > 40 {
+			http.Error(w, "invalid version", http.StatusBadRequest)
+			return
+		}
+		version = qrcode.Version(v)
+	}
+
+	qr, err := qrcode.New([]byte(data), opts...)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	if version > qr.Version {
+		qr.Version = version
+	}
+
+	img, err := qr.Encode(opts...)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	var buf bytes.Buffer
+	if err := png.Encode(&buf, img); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "image/png")
+	w.Write(buf.Bytes())
 }
